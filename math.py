a87cd76098b58deeb5aab4b89cdc40b7781b3d9a
Mompati Erold Keetile <43243167+Patco-Erold@users.noreply.github.com>
2021-03-14 21:44:40 +0200

    Updated the divide function
    Added a raise exception when the divisor is zero on the divide function.

--- math.py
+++ math.py
@@ -5,7 +5,9 @@ def subtract(x, y):
 	return x-y
 
 def divide(x, y):
-	return x/y if y != 0 else "Division by zero."
+	if y == 0:
+		raise ValueError("Can not divide by zero")
+	return x/y
 
 def multiply(x, y):
 	return x * y
